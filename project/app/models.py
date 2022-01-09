ae34d456f3fdc609aca96a85c59f9eb9413f597c
can <canbolukbas98@gmail.com>
2022-01-09 14:11:01 +0300

    add app into install apps & add models

--- project/app/models.py
+++ project/app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class CPURequest(models.Model):
+    timestamp = models.DateTimeField(primary_key=True, auto_now_add=True, editable=False)
+
+class MemoryRequest(models.Model):
+    timestamp = models.DateTimeField(primary_key=True, auto_now_add=True, editable=False)
