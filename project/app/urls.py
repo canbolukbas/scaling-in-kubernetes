fb18b860343e803ae2ea1c61c5a5f5bfade49497
can <canbolukbas98@gmail.com>
2022-01-09 19:04:07 +0300

    delete not used view references

--- project/app/urls.py
+++ project/app/urls.py
@@ -1,12 +1,10 @@
 from django.urls import path
 
-from app.views import CPUView, MemoryView, IOView, HealthView, CPURequestView, MemoryRequestView
+from app.views import CPUView, MemoryView, IOView, HealthView
 
 urlpatterns = [
     path('cpu/', CPUView.as_view()),
-    path('cpu-requests/', CPURequestView.as_view()),
     path('memory/', MemoryView.as_view()),
-    path('memory-requests/', MemoryRequestView.as_view()),
     path('io/', IOView.as_view()),
     path('health/', HealthView.as_view()),
 ]
--- project/app/views.py
+++ project/app/views.py
@@ -1,7 +1,3 @@
-from datetime import timedelta
-
-from django.utils import timezone
-
 from rest_framework.response import Response
 from rest_framework.status import HTTP_200_OK
 from rest_framework.views import APIView
