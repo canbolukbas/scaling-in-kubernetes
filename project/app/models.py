215ba53d54512a909ee15ca7ccd4baa4c5490d5c
can <canbolukbas98@gmail.com>
2022-01-11 23:14:44 +0300

    improve memory task & delete unnecessary models

--- project/app/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from django_prometheus.models import ExportModelOperationsMixin
-
-# Create your models here.
-class CPURequest(ExportModelOperationsMixin("cpu-intensive-requests"), models.Model):
-    timestamp = models.DateTimeField(primary_key=True, auto_now_add=True, editable=False)
-
-class MemoryRequest(ExportModelOperationsMixin("memory-intensive-requests"), models.Model):
-    timestamp = models.DateTimeField(primary_key=True, auto_now_add=True, editable=False)
--- project/app/views.py
+++ project/app/views.py
@@ -1,11 +1,9 @@
+from time import sleep
+
 from rest_framework.response import Response
 from rest_framework.status import HTTP_200_OK
 from rest_framework.views import APIView
 from prometheus_client import Summary
-
-from app.models import CPURequest
-from app.models import MemoryRequest
-
 
 # Create a metric to track time spent and requests made.
 REQUEST_SUMMARY = Summary('http_requests_total', 'Time spent processing request')
@@ -24,8 +22,6 @@ class CPUView(APIView):
         for i in range(pow(10, 4)):
             pass
         data = {"msg": "Completed"}
-        cpu_request = CPURequest()
-        cpu_request.save()
         return Response(data=data, status=HTTP_200_OK)
 
 
@@ -33,10 +29,9 @@ class MemoryView(APIView):
 
     @REQUEST_SUMMARY.time()
     def get(self, request, *args, **kwargs):
-        memory_intensive_task = ['Cloud'*1024 for _ in range(0, 1024)]
+        memory_intensive_task = bytearray(37159485)
+        sleep(1)
         data = {"msg": "Completed"}
-        memory_request = MemoryRequest()
-        memory_request.save()
         return Response(data=data, status=HTTP_200_OK)
 
 
