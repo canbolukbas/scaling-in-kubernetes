af5b189e31903c0abf4810a7bacfeb1834f599a4
Muhammet Yazici <muhammet.yazici@hazelcast.com>
2022-01-12 19:41:45 +0300

    Add go application for load testing

---
+++ go-project/go.mod
@@ -0,0 +1,3 @@
+module kubernetes-app
+
+go 1.16
---
+++ go-project/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"net/http"
+	"time"
+)
+
+func health(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(w, "done\n")
+}
+
+func cpu(w http.ResponseWriter, req *http.Request) {
+	for i := 1; i <= int(math.Pow(10, 4)); i++ {
+	}
+	fmt.Fprintf(w, "done\n")
+}
+
+func memory(w http.ResponseWriter, req *http.Request) {
+	_ = make([]byte, 50000)
+	time.Sleep(500 * time.Millisecond)
+	fmt.Fprintf(w, "done\n")
+}
+
+func main() {
+
+	http.HandleFunc("/cpu", cpu)
+	http.HandleFunc("/memory", memory)
+	http.HandleFunc("/health", health)
+
+	http.ListenAndServe(":8080", nil)
+}
