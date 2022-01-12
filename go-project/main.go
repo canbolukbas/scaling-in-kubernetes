fd41f6f781872bf9069085ab7926296c14a132d4
Muhammet Yazici <muhammet.yazici@hazelcast.com>
2022-01-12 21:46:03 +0300

    Add custom metrics collector

--- go-project/main.go
+++ go-project/main.go
@@ -5,6 +5,17 @@ import (
 	"math"
 	"net/http"
 	"time"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+var (
+	opsProcessed = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "http_requests_total",
+		Help: "The total number of requests",
+	})
 )
 
 func health(w http.ResponseWriter, req *http.Request) {
@@ -12,12 +23,14 @@ func health(w http.ResponseWriter, req *http.Request) {
 }
 
 func cpu(w http.ResponseWriter, req *http.Request) {
+	opsProcessed.Inc()
 	for i := 1; i <= int(math.Pow(10, 4)); i++ {
 	}
 	fmt.Fprintf(w, "done\n")
 }
 
 func memory(w http.ResponseWriter, req *http.Request) {
+	opsProcessed.Inc()
 	go func() {
 		_ = make([]byte, 200000)
 		time.Sleep(500 * time.Millisecond)
@@ -31,6 +44,6 @@ func main() {
 	http.HandleFunc("/cpu", cpu)
 	http.HandleFunc("/memory", memory)
 	http.HandleFunc("/health", health)
-
+	http.Handle("/metrics", promhttp.Handler())
 	http.ListenAndServe(":8080", nil)
 }
