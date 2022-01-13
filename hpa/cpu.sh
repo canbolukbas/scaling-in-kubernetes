f57dcb4aa06744dd103600104adc0b626c80b58e
can <canbolukbas98@gmail.com>
2022-01-13 11:15:22 +0300

    add kubectl note for me

--- hpa/cpu.sh
+++ hpa/cpu.sh
@@ -5,6 +5,9 @@ kubectl apply -f manifests/app/cpu -n cpu
 
 #kubectl config set-context --current --namespace=cpu
 
+# without pod autoscaling
+#kubectl delete -f manifests/app/cpu/hpa-cpu.yaml    
+
 # TO DELETE
 kubectl delete -f manifests/app/cpu -n cpu
 kubectl delete namespace cpu
